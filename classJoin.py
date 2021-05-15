23a1adf3b05337c7fedbb513900fbd894a6831dd
Deepak Pattnayak <deepakpattnayak2013@gmail.com>
2021-05-15 18:56:48 +0530

    Update classJoin.py

--- classJoin.py
+++ classJoin.py
@@ -26,11 +26,11 @@ try:
     login_btn.click()
     email_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "identifierId")))
     time.sleep(2)
-    email_input.send_keys()
+    email_input.send_keys("deepakpattnayak2013@gmail.com")
     email_input.send_keys(Keys.ENTER)
     pass_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "password")))
     time.sleep(2)
-    pass_input.send_keys()
+    pass_input.send_keys("gofuckyourself")
     pass_input.send_keys(Keys.ENTER)
     print("Logged in into Gmail account")
 except Exception as e:
@@ -38,7 +38,7 @@ except Exception as e:
 
 driver.get(URL)
 
-print("Muting mic and camera...")
+
 try:
         # close popup
     dismiss = WebDriverWait(driver, 10).until(
@@ -51,18 +51,19 @@ try:
     mic = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
             (By.XPATH, "/html/body/div[1]/c-wiz/div/div/div[7]/div[3]/div/div/div[2]/div/div[1]/div["
                        "1]/div/div[4]/div[1]/div/div/div")))
+    
     mic.click()
-    print("Muted mic and camera")
 except Exception as e:
     print(e)
     
-print("Joining meeting...")
+
 try:
     join_now = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,
                                                                                   "/html/body/div[1]/c-wiz/div/div/div[7]/div[3]/div/div/div[2]/div/div[1]/div[2]/div/div[2]/div/div[1]/div[1]/span")))
     join_now.click()
 except Exception as e:
         print(e)   
+
 
 
 
