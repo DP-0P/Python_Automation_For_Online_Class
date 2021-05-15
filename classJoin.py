3ee71385817e5bb578b42e6b9f843a3b61dfaf3c
Deepak Pattnayak <deepakpattnayak2013@gmail.com>
2021-05-15 18:36:58 +0530

    Update classJoin.py

--- classJoin.py
+++ classJoin.py
@@ -1,5 +1,68 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.alert import Alert
+import time
+import subprocess
+import webbrowser
 
+firefox_options = Options()
+firefox_options.add_argument("start-maximized")
+firefox_options.add_argument("disable-popup-blocking")
+firefox_options.add_argument("--disable-notifications")
+driver = webdriver.Firefox(executable_path=r'C:\\Users\\user\\Desktop\\Python Automation\\geckodriver.exe')
+driver.maximize_window()
 
+URL = "https://meet.google.com/fxf-cwxo-tui"
+
+url = "https://stackoverflow.com/users/login"
+driver.get(url)
+try:
+    login_btn = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "s-btn__google")))
+    time.sleep(2)
+    login_btn.click()
+    email_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "identifierId")))
+    time.sleep(2)
+    email_input.send_keys()
+    email_input.send_keys(Keys.ENTER)
+    pass_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "password")))
+    time.sleep(2)
+    pass_input.send_keys()
+    pass_input.send_keys(Keys.ENTER)
+    print("Logged in into Gmail account")
+except Exception as e:
+    print(e)
+
+driver.get(URL)
+
+print("Muting mic and camera...")
+try:
+        # close popup
+    dismiss = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "/html/body/div/div[3]/div/div[2]/div[3]/div/span/span")))
+    dismiss.click()
+        # dismiss alert
+    alert = Alert(driver)
+    alert.dismiss()
+        # mute mic
+    mic = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
+            (By.XPATH, "/html/body/div[1]/c-wiz/div/div/div[7]/div[3]/div/div/div[2]/div/div[1]/div["
+                       "1]/div/div[4]/div[1]/div/div/div")))
+    mic.click()
+    print("Muted mic and camera")
+except Exception as e:
+    print(e)
+    
+print("Joining meeting...")
+try:
+    join_now = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,
+                                                                                  "/html/body/div[1]/c-wiz/div/div/div[7]/div[3]/div/div/div[2]/div/div[1]/div[2]/div/div[2]/div/div[1]/div[1]/span")))
+    join_now.click()
+except Exception as e:
+        print(e)   
 
 
 
