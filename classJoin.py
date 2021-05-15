c247b6b969999cbd3cdfa5ff9e6293ee338f7404
Deepak Pattnayak <deepakpattnayak2013@gmail.com>
2021-05-15 20:22:55 +0530

    Update classJoin.py

--- classJoin.py
+++ classJoin.py
@@ -29,11 +29,11 @@ try:
     login_btn.click()
     email_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "identifierId")))
     time.sleep(2)
-    email_input.send_keys("deepakpattnayak2013@gmail.com")
+    email_input.send_keys(#enter Gmail ID)
     email_input.send_keys(Keys.ENTER)
     pass_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "password")))
     time.sleep(2)
-    pass_input.send_keys("gofuckyourself")
+    pass_input.send_keys(#enter Password)
     pass_input.send_keys(Keys.ENTER)
     print("Logged in into Gmail account")
 except Exception as e:
